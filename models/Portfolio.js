e3e5d1385f681fe139201e810ed773a1ceab0263
Dave Regg <drregg6@gmail.com>
2019-10-23 13:48:48 -0400

    Portfolio Schema complete

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -5,6 +5,23 @@ const portfolioSchema = new Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  technologies: {
+    type: [String]
+  },
+  image: {
+    type: String
+  },
+  desc: {
+    type: String
   }
 });
 
