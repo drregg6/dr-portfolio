d2fd322237bfdc0a3b682e6623c8e50a31927a8e
Dave Regg <drregg6@gmail.com>
2019-10-23 19:59:13 -0400

    Added loadUser to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Header from './components/layout/Header';
 import Portfolio from './components/portfolio/Portfolio';
 import Footer from './components/layout/Footer';
 
+import setAuthToken from './utils/setAuthToken';
+import loadUser from './actions/auth';
+import store from './store';
+
 function App() {
+  useEffect(() => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+  }, [])
   return (
     <div className="App">
       <Header />
