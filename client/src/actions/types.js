7eb6c301f8efe92a3e92c938cc6a3c09562cb605
Dave Regg <drregg6@gmail.com>
2019-10-23 19:55:43 -0400

    Auth and alert should be complete

---
+++ client/src/actions/types.js
@@ -0,0 +1,14 @@
+// Alert types
+
+
+// Auth types
+const USER_LOADED = 'USER_LOADED';
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+const LOGIN_FAIL = 'LOGIN_FAIL';
+const LOGOUT_USER = 'LOGOUT_USER';
+const AUTH_ERROR = 'AUTH_ERROR';
+
+// Portfolio types
+
+
+// Resume types
