0cd1d43e6fdc1d4515334e7aa89b7f00820fbbe3
Dave Regg <drregg6@gmail.com>
2019-11-06 17:30:03 -0500

    Navigation works on mobile and desktop

--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -17,54 +17,54 @@ const Nav = ({
     fetchResume();
   }, [])
   return (
-    <div className="nav top-nav">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/resume">Resume</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          { isAuthenticated && (
-            <Fragment>
-              <li>
-                <Link to="/new-portfolio">New Portfolio</Link>
-              </li>
-              {
-                !resume ? (
-                  <Fragment>
-                    <li>
-                      <Link to="/new-resume">New Resume</Link>
-                    </li>
-                  </Fragment>
-                ) : (
-                  <Fragment>
-                    <li>
-                      <Link to="/edit-resume">Edit Resume</Link>
-                    </li>
-                  </Fragment>
-                )
-              }
-              <li>
-                <Link to="new-employment">Add Employer</Link>
-              </li>
-              <li>
-                <Link to="new-experience">Add Experience</Link>
-              </li>
-              <li>
-                <Link to="new-education">Add Education</Link>
-              </li>
-            </Fragment>
-          ) }
-        </ul>
-      </nav>
-      <a href="#" className="nav-icon" onClick={() => responsiveNav()}>
-        <i className="fa fa-bars"></i>
-      </a>
+    <div className="nav">
+      <div className="nav-toggle">
+        <a href="#!" className="nav-icon" onClick={() => responsiveNav()}>
+          Menu
+        </a>
+      </div>
+      <div className="nav-link">
+      <div>
+        <Link to="/">Portfolio</Link>
+      </div>
+      <div>
+        <Link to="/resume">Resume</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+      </div>
+      { isAuthenticated && (
+        <Fragment>
+          <div>
+            <Link to="/new-portfolio">New Portfolio</Link>
+          </div>
+          {
+            !resume ? (
+              <Fragment>
+                <div>
+                  <Link to="/new-resume">New Resume</Link>
+                </div>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <div>
+                  <Link to="/edit-resume">Edit Resume</Link>
+                </div>
+              </Fragment>
+            )
+          }
+          <div>
+            <Link to="new-employment">Add Employer</Link>
+          </div>
+          <div>
+            <Link to="new-experience">Add Experience</Link>
+          </div>
+          <div>
+            <Link to="new-education">Add Education</Link>
+          </div>
+        </Fragment>
+      ) }
+      </div>
       <Alert />
     </div>
   )
--- client/src/utils/responsiveNav.js
+++ client/src/utils/responsiveNav.js
@@ -1,9 +1,4 @@
 export default function() {
-  let x = document.querySelector('.top-nav');
-  console.log(x);
-  if (x.className === 'top-nav nav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'top-nav nav';
-  }
+  let navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(link => link.classList.toggle('navbar-show'));
 }
